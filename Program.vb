bd785ba76079d60e4e704826b5c430170d9bd6e5
Jacob Mellor <developers@ironsoftware.com>
2018-09-21 10:48:17 +0700

    Initial commit
    See https://ironpdf.com/docs/questions/csharp-html-to-pdf-example-file-and-code/

---
+++ Program.vb
@@ -0,0 +1,48 @@
+﻿'========================================================================
+' This conversion was produced by the Free Edition of
+' Instant VB courtesy of Tangible Software Solutions.
+' Order the Premium Edition at https://www.tangiblesoftwaresolutions.com
+'========================================================================
+
+Imports System
+Imports System.Collections.Generic
+Imports System.Diagnostics
+Imports System.IO
+Imports System.Linq
+Imports System.Text
+Imports System.Threading.Tasks
+Imports IronPdf
+
+Namespace IronPDFDemo
+	Friend Class Program
+		Shared Sub Main(ByVal args() As String)
+			Console.WriteLine("Hold on tight!")
+
+			Dim htmlToPdf = New HtmlToPdf() ' new instance of HtmlToPdf
+
+			'html to pdf
+			'html to turn into pdf
+			Dim html = "<h1>Hello World!</h1><br><p>This is IronPdf.</p>"
+
+			' turn html to pdf
+			Dim pdf = htmlToPdf.RenderHtmlAsPdf(html)
+
+			' save resulting pdf into file
+			pdf.SaveAs(Path.Combine(Directory.GetCurrentDirectory(), "HtmlToPdf.Pdf"))
+
+			'url to pdf
+			' uri of the page to turn into pdf
+			Dim uri = New Uri("http://www.google.com/ncr")
+
+			' turn page into pdf
+			pdf = htmlToPdf.RenderUrlAsPdf(uri)
+
+			' save resulting pdf into file
+			pdf.SaveAs(Path.Combine(Directory.GetCurrentDirectory(), "UrlToPdf.Pdf"))
+
+			Console.WriteLine("Done. Please find results under '{0}' directory.", Directory.GetCurrentDirectory())
+			Console.WriteLine("Press any key to continue.")
+			Console.ReadKey()
+		End Sub
+	End Class
+End Namespace
---
+++ README.md
@@ -0,0 +1,2 @@
+# iron-pdf-example-hello-world-vb.net
+Get started creating PDF documents in Visual Basic .Net using IronPDF - https://ironpdf.com/
